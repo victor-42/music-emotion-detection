275557189d71de98b96f5c60c0dad50d95fd52d7
Victor Hug <info@victorhug.de>
2022-05-12 01:12:46 +0200

    rnn

--- customdataset.py
+++ customdataset.py
@@ -9,10 +9,28 @@ import warnings
 
 
 class WindowedAudioDataset(Dataset):
+    """
+
+    """
     def __init__(self, arousal_file, valence_file, audio_dir,
                  window_length=20.0, window_hopsize=15.0,
                  sampling_rate=44100, max_length=45.0, mfcc_features=30,
-                 feature_hop_len=1764, feature_window_size=4410):
+                 feature_hop_len=1764, feature_window_size=4410,
+                 sequence_annotation=True):
+        """
+
+        :param arousal_file:
+        :param valence_file:
+        :param audio_dir:
+        :param window_length:
+        :param window_hopsize:
+        :param sampling_rate:
+        :param max_length:
+        :param mfcc_features:
+        :param feature_hop_len:
+        :param feature_window_size:
+        :param sequence_annotation:
+        """
         self.mfcc_features = mfcc_features
         self.audio_dir = audio_dir
         self.sr = sampling_rate
@@ -28,6 +46,7 @@ class WindowedAudioDataset(Dataset):
         self.mfccs = []
         self.tempograms = []
         self.energies = []
+        self.sequence_annotation = sequence_annotation
 
         # self.target_transform = target_transform
         # self.transform = transform
@@ -40,23 +59,37 @@ class WindowedAudioDataset(Dataset):
         print('Retrieving File Names and Labels...')
         max_range = (self.annotation_end - self.window_length / 2) * 1000 + 1
 
-        for i, row in tqdm(self.valence_file.iterrows()):
-            self.file_names.append(str(int(row['song_id'])) + '.mp3')
+        if self.sequence_annotation:
+            for i, row in tqdm(self.valence_file.iterrows()):
+                self.file_names.append(str(int(row['song_id'])) + '.mp3')
+                max_range -= 1
+                for start_index in range(int(self.annotation_start * 1000), int((self.annotation_end-self.window_length) * 1000),
+                                         int(self.window_length * 1000)):
+                    label = []
+                    for point in range(start_index, int(start_index + self.window_length * 1000), 500):
+                        label_name = 'sample_' + str(point) + 'ms'
+                        label.append([row[label_name], self.arousal_file.iloc[i].loc[label_name]])
 
-            for label_index in range(int(self.annotation_start * 1000), int(max_range),
-                                     int(self.window_hopsize * 1000)):
-                label_name = 'sample_' + str(label_index) + 'ms'
-                self.labels.append([row[label_name], self.arousal_file.iloc[i].loc[label_name]])
+                    self.labels.append(label)
+
+        else:
+            for i, row in tqdm(self.valence_file.iterrows()):
+                self.file_names.append(str(int(row['song_id'])) + '.mp3')
+
+                for label_index in range(int(self.annotation_start * 1000), int(max_range),
+                                         int(self.window_hopsize * 1000)):
+                    label_name = 'sample_' + str(label_index) + 'ms'
+                    self.labels.append([row[label_name], self.arousal_file.iloc[i].loc[label_name]])
 
         self.labels = np.array(self.labels)
         print('Done!')
 
         print('Loading Files and Features...')
+        print(f'Using {worker} Threads')
         warnings.filterwarnings('ignore', '.*audioread.*')
         # self.samples = []
 
         n_per_second = int(self.sr / self.feature_hop_len)
-
         self.mfccs = []
         self.tempograms = []
         self.energies = []
@@ -64,14 +97,23 @@ class WindowedAudioDataset(Dataset):
         res = Parallel(n_jobs=worker, backend='multiprocessing')(
             delayed(self.load_sample)(f) for f in tqdm(self.file_names))
 
-        range_max = int(self.annotation_end - self.window_length / 2)
-        for i in tqdm(range(len(self.file_names))):
-            for ii in range(int(self.annotation_start), range_max, int(self.window_hopsize)):
-                start = int((ii - self.window_length / 2) * n_per_second)
-                end = int((ii + self.window_length / 2) * n_per_second)
-                self.mfccs.append(res[i][0][start:end])
-                self.energies.append(res[i][1][start:end])
-                self.tempograms.append(res[i][2][start:end])
+        if self.sequence_annotation:
+            for i in tqdm(range(len(self.file_names))):
+                for start_index in range(int(self.annotation_start), int(self.annotation_end-self.window_length), int(self.window_length)):
+                    start = int(start_index * n_per_second)
+                    end = int((start_index + self.window_length) * n_per_second)
+                    self.mfccs.append(res[i][0][start:end])
+                    self.energies.append(res[i][2][start:end])
+                    self.tempograms.append(res[i][2][start:end])
+        else:
+            range_max = int(self.annotation_end - self.window_length / 2)
+            for i in tqdm(range(len(self.file_names))):
+                for ii in range(int(self.annotation_start), range_max, int(self.window_hopsize)):
+                    start = int((ii - self.window_length / 2) * n_per_second)
+                    end = int((ii + self.window_length / 2) * n_per_second)
+                    self.mfccs.append(res[i][0][start:end])
+                    self.energies.append(res[i][1][start:end])
+                    self.tempograms.append(res[i][2][start:end])
 
         self.mfccs = np.array(self.mfccs)
         self.tempograms = np.array(self.tempograms)
